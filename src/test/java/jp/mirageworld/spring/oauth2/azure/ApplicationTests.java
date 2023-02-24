4d23de31513519f945804c6cf2d5857c91c20ebb
hnuma1979 <118696808+hnuma1979@users.noreply.github.com>
2023-02-24 22:15:44 +0900

    Spring Initialize

---
+++ src/test/java/jp/mirageworld/spring/oauth2/azure/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.mirageworld.spring.oauth2.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
