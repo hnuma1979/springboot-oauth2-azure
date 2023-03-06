b4892ca5566133218f9d4de7100fc7e94470d996
hnuma1979 <118696808+hnuma1979@users.noreply.github.com>
2023-03-06 10:25:37 +0900

    承認・辞退・キャンセル・転送等

--- src/main/java/jp/mirageworld/spring/oauth2/azure/service/EventGraphService.java
+++ src/main/java/jp/mirageworld/spring/oauth2/azure/service/EventGraphService.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 import org.springframework.util.Assert;
 
+import com.microsoft.graph.models.DateTimeTimeZone;
 import com.microsoft.graph.models.Event;
+import com.microsoft.graph.models.EventAcceptParameterSet;
+import com.microsoft.graph.models.EventCancelParameterSet;
+import com.microsoft.graph.models.EventDeclineParameterSet;
+import com.microsoft.graph.models.EventForwardParameterSet;
+import com.microsoft.graph.models.EventSnoozeReminderParameterSet;
+import com.microsoft.graph.models.EventTentativelyAcceptParameterSet;
+import com.microsoft.graph.models.Recipient;
+import com.microsoft.graph.models.TimeSlot;
 import com.microsoft.graph.options.Option;
 import com.microsoft.graph.requests.EventCollectionPage;
 import com.microsoft.graph.requests.EventCollectionRequestBuilder;
@@ -51,6 +60,96 @@ public class EventGraphService
     @Override
     public EventCollectionPage list(Option... options) {
         return this.list(List.of(options));
+    }
+
+    /**
+     * 承認
+     */
+    public void accept(@Nonnull String id, String comment, boolean response) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).accept(
+                EventAcceptParameterSet.newBuilder().withComment(comment).withSendResponse(response).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * 仮承認
+     */
+    public void tentativelyAccept(@Nonnull String id, String comment, boolean response) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).tentativelyAccept(
+                EventTentativelyAcceptParameterSet.newBuilder().withComment(comment).withSendResponse(response).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * 仮承認（時間の提案）
+     */
+    public void tentativelyAccept(@Nonnull String id, String comment, TimeSlot time) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).tentativelyAccept(
+                EventTentativelyAcceptParameterSet.newBuilder()
+                        .withComment(comment).withSendResponse(true).withProposedNewTime(time).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * 転送
+     */
+    public void forward(@Nonnull String id, String comment, List<Recipient> recipients) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).forward(
+                EventForwardParameterSet.newBuilder().withComment(comment).withToRecipients(recipients).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * キャンセル
+     */
+    public void cancel(@Nonnull String id, String comment) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).cancel(
+                EventCancelParameterSet.newBuilder().withComment(comment).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * 辞退
+     */
+    public void decline(@Nonnull String id, String comment, boolean response) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).decline(
+                EventDeclineParameterSet.newBuilder().withComment(comment).withSendResponse(response).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * 辞退（時間の提案）
+     */
+    public void decline(@Nonnull String id, String comment, TimeSlot time) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).decline(
+                EventDeclineParameterSet.newBuilder()
+                        .withComment(comment).withSendResponse(true).withProposedNewTime(time).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * リマインダー
+     */
+    public void snoozeReminder(@Nonnull String id, DateTimeTimeZone reminder) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).snoozeReminder(
+                EventSnoozeReminderParameterSet.newBuilder().withNewReminderTime(reminder).build())
+                .buildRequest().post();
+    }
+
+    /**
+     * リマインダー
+     */
+    public void dismissReminder(@Nonnull String id, DateTimeTimeZone reminder) {
+        Assert.notNull(id, "id");
+        this.builder.byId(id).dismissReminder().buildRequest().post();
     }
 
 }
