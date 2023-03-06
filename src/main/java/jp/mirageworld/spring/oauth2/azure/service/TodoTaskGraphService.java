3f053a9c3d0b232a2818d48b32fdd3dc27ef6cfe
hnuma1979 <118696808+hnuma1979@users.noreply.github.com>
2023-03-06 10:55:35 +0900

    todo task api

--- src/main/java/jp/mirageworld/spring/oauth2/azure/service/TodoTaskGraphService.java
+++ src/main/java/jp/mirageworld/spring/oauth2/azure/service/TodoTaskGraphService.java
@@ -4,57 +4,53 @@ import java.util.List;
 
 import org.springframework.util.Assert;
 
-import com.microsoft.graph.models.TodoTaskList;
+import com.microsoft.graph.models.TodoTask;
 import com.microsoft.graph.options.Option;
-import com.microsoft.graph.requests.TodoTaskListCollectionPage;
-import com.microsoft.graph.requests.TodoTaskListCollectionRequestBuilder;
+import com.microsoft.graph.requests.TodoTaskCollectionPage;
+import com.microsoft.graph.requests.TodoTaskCollectionRequestBuilder;
 
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class TodoTaskListGraphService
+public class TodoTaskGraphService
         implements
-        ICrudGraphService<TodoTaskList, TodoTaskListCollectionPage> {
+        ICrudGraphService<TodoTask, TodoTaskCollectionPage> {
 
-    final TodoTaskListCollectionRequestBuilder builder;
+    final TodoTaskCollectionRequestBuilder builder;
 
     @Override
-    public TodoTaskList create(@Nonnull TodoTaskList item) {
+    public TodoTask create(@Nonnull TodoTask item) {
         Assert.notNull(item, "item");
         return this.builder.buildRequest().post(item);
     }
 
     @Override
-    public TodoTaskList update(@Nonnull TodoTaskList item) {
+    public TodoTask update(@Nonnull TodoTask item) {
         Assert.notNull(item, "item");
         return this.builder.byId(item.id).buildRequest().patch(item);
     }
 
     @Override
-    public TodoTaskList delete(@Nonnull TodoTaskList item) {
+    public TodoTask delete(@Nonnull TodoTask item) {
         Assert.notNull(item, "item");
         return this.builder.byId(item.id).buildRequest().delete();
     }
 
     @Override
-    public TodoTaskList get(@Nonnull String id) {
+    public TodoTask get(@Nonnull String id) {
         Assert.notNull(id, "id");
         return this.builder.byId(id).buildRequest().get();
     }
 
     @Override
-    public TodoTaskListCollectionPage list(List<Option> options) {
+    public TodoTaskCollectionPage list(List<Option> options) {
         return this.builder.buildRequest(options).get();
     }
 
     @Override
-    public TodoTaskListCollectionPage list(Option... options) {
+    public TodoTaskCollectionPage list(Option... options) {
         return this.list(List.of(options));
-    }
-
-    public TodoTaskGraphService tasks(String id) {
-        return new TodoTaskGraphService(this.builder.byId(id).tasks());
     }
 
 }
